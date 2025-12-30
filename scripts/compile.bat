dbd85d83f0d0bad97bca4d5a5019ebdb34cd6633
yoelgarcialago <yoelgarcialago@gmail.com>
2025-12-30 21:33:20 +0100

    Recursive compile script

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,15 +1,29 @@
 @echo off
-echo Compilando proyecto COBOL...
+echo Compilando programas COBOL...
 
+REM Ruta al config de GnuCOBOL
 set COB_CONFIG_PATH=C:\msys64\mingw64\share\gnucobol\config
-set SRC=src\main.cob
-set OUT=bin\main.exe
 
-if not exist bin (
-    mkdir bin
+REM Carpetas
+set SRC_DIR=src
+set BIN_DIR=bin
+set COPY_DIR=copybooks
+
+REM Crear carpeta bin si no existe
+if not exist %BIN_DIR% (
+    mkdir %BIN_DIR%
 )
 
-cobc -x -conf=%COB_CONFIG_PATH%\default.conf -o %OUT% %SRC%
+REM Bucle por cada archivo .cob en src
+for %%F in (%SRC_DIR%\*.cob) do (
+    echo Compilando %%~nxF ...
+    cobc -x ^
+        -conf=%COB_CONFIG_PATH%\default.conf ^
+        -o %BIN_DIR%\%%~nF.exe ^
+        %%F ^
+        -I %COPY_DIR%
+)
 
+echo.
 echo Compilación finalizada
 pause
